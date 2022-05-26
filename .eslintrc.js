63622cff2957aa3c89e422932703c408d280cb22
zFernand0 <fernando.rijocedeno@broadcom.com>
2022-05-26 20:57:05 +0000

    Eslint Updates
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,21 +8,45 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:jest/recommended"
     ],
+    "ignorePatterns": [
+        "**/*.js",
+        "**/*.d.ts"
+    ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": 11,
         "sourceType": "module"
     },
     "plugins": [
-        "@typescript-eslint"
+        "@typescript-eslint",
+        "license-header",
+        "unused-imports"
     ],
     "rules": {
+        "max-len": ["warn", 150],
+        "no-console": "error",
+        "no-multiple-empty-lines": "warn",
+        "no-trailing-spaces": "warn",
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/comma-dangle": ["warn", "only-multiline"],
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/indent": ["warn", 4],
         "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-unused-vars": ["warn",
-            {
-                "argsIgnorePattern": "^_"
-            }
-        ],
-        "@typescript-eslint/no-var-requires": "off"
+        "@typescript-eslint/no-inferrable-types": "off",
+        "@typescript-eslint/no-magic-numbers": ["warn", {
+            "ignore": [-2, -1, 0, 1, 2, 3],
+            "ignoreDefaultValues": true,
+            "ignoreReadonlyClassProperties": true
+        }],
+        "@typescript-eslint/no-unused-vars": ["warn", {
+            "argsIgnorePattern": "^_"
+        }],
+        "license-header/header": ["error", "./LICENSE_HEADER"],
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/semi": "warn",
+        "unused-imports/no-unused-imports": "warn",
+        "unused-imports/no-unused-vars": ["warn", {
+            "args": "none"
+        }]
     }
 };
